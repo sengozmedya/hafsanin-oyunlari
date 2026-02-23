c38e94d28fd6b12a90897be7ecd7899af04dd450
SENGOZMEDYA <sengozmedya@SENGOZMEDYA-Air.hgw.local>
2026-02-23 03:02:54 +0300

    chore: configure nextjs for static html export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  // basePath: '', // Eğer bir alt klasörde çalışacaksa buraya klasör adı girilmelidir
 };
 
 export default nextConfig;
